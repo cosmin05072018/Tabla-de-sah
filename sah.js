0237e72947f561104083b2abe5373e42e73c3889
Morari Cosmin <cosminmorari99@yahoo.com>
2022-03-31 17:56:04 -0700

    add new project

---
+++ sah.js
@@ -0,0 +1,11 @@
+const item = document.querySelectorAll(".item");
+/*
+for (let i = 0; i < item.length; i++) {
+  item[i].addEventListener("click", () => {
+    if (item % 2 === 0) {
+        item.style.backgroundColor = "black";
+      }
+  });
+}
+*/
+
