2a31bf7b8ae4172b9f6454f869c79d78997b959f
Morari Cosmin <cosminmorari99@yahoo.com>
2022-04-01 21:11:45 -0700

    delete boostrap

--- sah.js
+++ sah.js
@@ -1,24 +1,2 @@
-/*const piese = document.querySelectorAll(".item");
-console.log(piese);
-
-for (let i = 0; i < piese.length; i++){
-    piese[i].addEventListener("click", ()=>{
-        console.log("run");
-    })
-}
-
-const tura =  document.getElementById("tura");
-for (let i = 0; i < tura.length; i++){
-    tura[i].addEventListener("click", ()=>{
-        console.log("run");
-    })
-} 
-console.log(tura);
-*/
-const tura = document.getElementsByName("svg:not(:root).svg-inline--fa");
-console.log(tura);
-for (let i = 0; i < tura.length; i++){
-    tura[i].addEventListener("click", ()=>{
-        console.log("run");
-    })
-}
+const piesaSah = document.getElementsByTagName("i");
+console.log(piesaSah[1]);
