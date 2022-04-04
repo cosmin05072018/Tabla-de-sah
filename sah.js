80c1b6afbf520a8d841e5c5aea1036c0769b4987
Morari Cosmin <cosminmorari99@yahoo.com>
2022-04-03 22:29:34 -0700

    gata pe ziua de azi

--- sah.js
+++ sah.js
@@ -1,3 +1,5 @@
+/* AICI VOM PRELUA CLASELE DE TIP ROW(1,2,3,4,5,6,7,8), AJUTANDU-NE DE QUERYSELECTOR */
+
 const row1 = document.querySelector(".row1");
 const row2 = document.querySelector(".row2");
 const row3 = document.querySelector(".row3");
@@ -6,6 +8,17 @@ const row5 = document.querySelector(".row5");
 const row6 = document.querySelector(".row6");
 const row7 = document.querySelector(".row7");
 const row8 = document.querySelector(".row8");
+
+/* PANA AICI AVEM CLASELE DE TIP ROW */
+
+/* AICI VOM AVEA UN OBIECT NUMIT "chessTable" SI VOM AVEA URMATOARELE KEYS:
+                -row1, row2, row3, row4, row5, row6, row7, row8
+*** ROW1 VA AVEA CA SI VALOARE DENUMIRILE PIESELOR DE CULOARE ALBA INTR-UN ARRAY ***
+*** ROW2 VA AVEA CA SI VALOARE PIONII DE CULOARE ALBA ***
+*** ROW3, ROW4, ROW5, ROW6 VOR FI DECLARATE CA FIIND "NULL" DEOARECE NU AU NICIO PIESA/ELEMENT PE ELE ***
+*** ROW7 VA AVEA CA SI VALOARE PIONII DE CULOARE NEAGRA ***
+*** ROW1 VA AVEA CA SI VALOARE DENUMIRILE PIESELOR DE CULOARE NEAGRA INTR-UN ARRAY ***
+ */
 
 const chessTable = {
   row1: [
@@ -35,6 +48,10 @@ const chessTable = {
     "turnNegru",
   ],
 };
+
+/* PANA AICI AVEM OBIECTUL NUMIT "chessTable" */
+
+/* AICI AVEM FUNCTION EXPRESSION NUMIT "decideChessPieceImg" CARE NE VA AJUTA(MAI JOS) PENTRU AFISAREA IMAGINILOR(SVG) */
 
 const decideChessPieceImg = (piece) => {
   switch (piece) {
